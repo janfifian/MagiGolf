eba6e2d8394bde85bddd34071725965af8a66b6d
Michal Mamona <michalha1174@gmail.com>
2018-10-24 15:12:56 +0200

    new ball movement system

--- Assets/Scripts/GolfBall.cs
+++ Assets/Scripts/GolfBall.cs
@@ -8,41 +8,78 @@ using UnityEngine.UI;
 
 public class GolfBall : MonoBehaviour {
 
-    public Transform ballstick;
     public bool hasBeenTouched;
     public Text displayComponent;
+    public Slider H_Slider;
+    public Slider V_Slider;
+    public Rigidbody rb;
+    public float MaxPower;
+
+    private int counter;
     private int count;
-    void Stop()
-    {
-        GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
-        GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0);
-    }
-	// Use this for initialization
-	void Start () {
-        Instantiate(ballstick, this.transform.position, new Quaternion(0, 0, 0, 0));
+    private float percentageOfHPower;
+    private float percentageOfVPower;
+    // Use this for initialization
+    void Start () {
         hasBeenTouched = false;
         count = 0;
         SetCounterText();
+        counter = 0;
     }
 	
 	// Update is called once per frame
-	void Update () {
-        
-        if(GetComponent<Rigidbody>().velocity.magnitude > 0.5)
+	void Update()
+    {
+        if (GetComponent<Rigidbody>().velocity.magnitude == 0)
         {
-        //    Debug.Log("Rolling");
-            hasBeenTouched = true;
-        }
+            H_Slider.GetComponent<SliderMovement>().Enable();
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                counter++;
+            }
+            if (counter == 1)
+            {
+                H_Slider.GetComponent<SliderMovement>().isEnabled = false;
+                percentageOfHPower = H_Slider.value;
+                V_Slider.GetComponent<SliderMovement>().Enable();
+            }
+            
+            if (counter == 2)
+            {
+                V_Slider.GetComponent<SliderMovement>().isEnabled = false;
+                percentageOfVPower = V_Slider.value;
+            }
 
-        if (hasBeenTouched && GetComponent<Rigidbody>().velocity.magnitude < 1 && GetComponent<Rigidbody>().velocity.magnitude > 0.0)
-        {
-        //    Debug.Log("Drop the base");
-            Stop();
-            Instantiate(ballstick, this.transform.position, new Quaternion(0,0,0,0));
-            hasBeenTouched = false;
-            count++;
-            SetCounterText();
+            if (counter == 3)
+            {
+                
+                rb.AddForce(Camera.main.transform.forward.x * MaxPower * percentageOfHPower,
+                    Camera.main.transform.forward.y * MaxPower * percentageOfVPower, 
+                    Camera.main.transform.forward.z * MaxPower * percentageOfHPower);
+                counter = 0;
+            }
         }
+        if (GetComponent<Rigidbody>().velocity.magnitude > 0)
+        {
+            H_Slider.GetComponent<SliderMovement>().Disable();
+            V_Slider.GetComponent<SliderMovement>().Disable();
+            H_Slider.GetComponent<SliderMovement>().isEnabled = true;
+            V_Slider.GetComponent<SliderMovement>().isEnabled = true;
+        }
+        
+
+        //if (GetComponent<Rigidbody>().velocity.magnitude > 0.5)
+        //{
+        //    hasBeenTouched = true;
+        //}
+
+        //if (hasBeenTouched && GetComponent<Rigidbody>().velocity.magnitude < 1 && GetComponent<Rigidbody>().velocity.magnitude > 0.0)
+        //{
+        ////    Debug.Log("Drop the base");
+        //    hasBeenTouched = false;
+        //    count++;
+        //    SetCounterText();
+        //}
 
         if (Input.GetKeyDown("k"))
         {
---
+++ Assets/Scripts/UI/SliderMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderMovement : MonoBehaviour {
+
+    public bool isEnabled = true;
+    public float speed = 2;
+    private Slider slider;
+	// Use this for initialization
+	void Start () {
+        slider = GetComponent<Slider>();
+        slider.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isEnabled)
+        {
+            //Debug.Log((Mathf.Sin(Time.time * speed) + 1) / 2);
+            float sinusArgument = (Mathf.Sin(Time.time * speed) + 1) / 2;
+            slider.value = sinusArgument;
+        }
+    }
+
+    public void Enable()
+    {
+        slider.gameObject.SetActive(true);
+    }
+
+    public void Disable()
+    {
+        slider.gameObject.SetActive(false);
+    }
+}
